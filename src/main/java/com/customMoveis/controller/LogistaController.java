4f5f2e0492b997e1e310b1a24ddbb20def7bb793
Thauanny <thauanny.ramos.124@ufrn.edu.br>
2022-11-22 21:51:10 -0300

    response de lojista aprimorado

--- src/main/java/com/customMoveis/controller/LogistaController.java
+++ src/main/java/com/customMoveis/controller/LogistaController.java
@@ -1,13 +1,12 @@
 package com.customMoveis.controller;
 
-import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.customMoveis.model.lojista.Lojista;
 import com.customMoveis.service.LojistaService;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -18,15 +17,27 @@ public class LogistaController {
     @Autowired
     private LojistaService lojistaService;
 
-
     @GetMapping("/{id}")
     public ResponseEntity<?> lojista(@PathVariable Integer id) {
-        return ResponseEntity.ok(lojistaService.lojista(id));
+        try {
+            return ResponseEntity.ok(lojistaService.lojista(id));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("{\"message\": \"Nao foi possivel cadastrar lojista\"}");
+        }
+
     }
 
     @GetMapping("/all")
-    public List<Lojista> lojistas() {
-        return lojistaService.lojistas();
+    public ResponseEntity<?> lojistas() {
+
+        try {
+            return ResponseEntity.ok(lojistaService.lojistas());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("{\"message\": \"Lojista nao encontrado\"}");
+        }
+
     }
 
 }
